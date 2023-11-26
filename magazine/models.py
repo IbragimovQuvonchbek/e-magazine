79396f3fea0a456928ec7cdd470f4701f4ac0911
Quvonchbek <147556696+IbragimovQuvonchbek@users.noreply.github.com>
2023-11-26 17:43:54 +0500

    beckend finished

--- magazine/models.py
+++ magazine/models.py
@@ -5,6 +5,12 @@ from django.urls import reverse
 class Post(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    )
     price = models.PositiveIntegerField()
     photo = models.ImageField(upload_to='images/', blank=True, null=True)
 
