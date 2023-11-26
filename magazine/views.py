64522dd56e70cb673595aa06d5c3042ccc66c3b3
OtabekJurabekov <otabekjurabekov3@gmail.com>
2023-11-26 16:08:02 +0500

    create page css ready

--- magazine/views.py
+++ magazine/views.py
@@ -8,30 +8,7 @@ from .models import Post
 class HomePageView(ListView):
     model = Post
     template_name = 'home.html'
-    context_object_name = 'posts'
-    paginate_by = 5
-
-    def get_queryset(self):
-        return Post.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        posts = context['posts']
-        paginator = Paginator(posts, self.paginate_by)
-
-        page = self.request.GET.get('page')
-        try:
-            posts = paginator.page(page)
-        except PageNotAnInteger:
-            # If page is not an integer, deliver the first page.
-            posts = paginator.page(1)
-        except EmptyPage:
-            # If page is out of range (e.g. 9999), deliver the last page of results.
-            posts = paginator.page(paginator.num_pages)
-
-        context['posts'] = posts
-        return context
-
+    
 
 class CreatePostView(CreateView):
     model = Post
